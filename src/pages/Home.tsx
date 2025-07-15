9c71d7cbf63cf33717b07e4b66295b20621a9ee3
Rajshekhar Roy <raj.codehub@gmail.com>
2025-07-15 10:01:29 +0530

    Added the filter to show the section accordingly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -66,30 +66,39 @@ export default function Home() {
       {/* <button className=' flex justify-center border-1 hover:shadow-gray-500 text-center rounded-sm p-2'>Shop Now</button> */}
 
       {/* try to figure out how I combined it  */}
-      {sections.map((section) => (
-        <div key={section.key}>
-          <h1 className="text-3xl text-red-600 font-bold text-center py-2">
-            {section.title}
-          </h1>
-          <div className="flex justify-center">
-            <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 py-2">
-              {apiproduct
-                ?.filter((item) => item.section === section.key)
-                .map((item) => (
-                  <Link to={`/products/${item._id}`} key={item._id}>
-                    <Cards
-                      imageUrl={item.imageUrl}
-                      item1={item.item}
-                      description1={item.description}
-                      price1={item.price}
-                      handleClick={() => console.log("item added to cart")}
-                    />
-                  </Link>
-                ))}
+      {sections.map((section) => {
+        //i added this to filter it by checking if the section key which is here "trending" matches to the item.section means from the database item.section
+        const filteredProducts = apiproduct?.filter(
+          (item) => item.section === section.key
+        );
+        if (!filteredProducts || filteredProducts.length === 0) return null;
+
+        // here i added this return keyword after adding the curly brackets in the callback function in map
+        return (
+          <div key={section.key}>
+            <h1 className="text-3xl text-red-600 font-bold text-center py-2">
+              {section.title}
+            </h1>
+            <div className="flex justify-center">
+              <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 py-2">
+                {apiproduct
+                  ?.filter((item) => item.section === section.key)
+                  .map((item) => (
+                    <Link to={`/products/${item._id}`} key={item._id}>
+                      <Cards
+                        imageUrl={item.imageUrl}
+                        item1={item.item}
+                        description1={item.description}
+                        price1={item.price}
+                        handleClick={() => console.log("item added to cart")}
+                      />
+                    </Link>
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
