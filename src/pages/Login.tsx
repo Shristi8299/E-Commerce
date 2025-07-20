3159b2a2eceb50dd32861fbd1be34d1ca61e0c87
Rajshekhar Roy <raj.codehub@gmail.com>
2025-07-20 08:55:46 +0530

    added something

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 
 export default function Login() {
-  
+  const [error, setError] = useState<any>();
+  const [email, setEmail] = useState<any>("");
+  const user = {
+    email: "thanos@gmail.com",
+    password: "12345678",
+  };
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3000/login", user);
+      console.log(res);
+    } catch (error) {
+      setError(error.response.data.msg);
+    }
+  };
   return (
     <>
       <h1 className="font-bold text-center text-2xl mt-6">Log In</h1>
       <div className="mt-4  flex justify-center px-4 py-4 ">
-        <form className="w-full  max-w-md bg-white  py-15 p-6 rounded-xl   shadow-md hover:shadow-xl  space-y-4">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(e.target);
+          }}
+          className="w-full  max-w-md bg-white  py-15 p-6 rounded-xl   shadow-md hover:shadow-xl  space-y-4"
+        >
           <div>
             <input
-              required
-              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               placeholder="Enter email"
               className="w-full h-12 border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-1 focus:ring-black"
             />
@@ -17,11 +39,10 @@ export default function Login() {
 
           <div>
             <input
-              required
-              type="number"
               placeholder="Password"
               className="w-full  h-12 border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-1 focus:ring-black"
             />
+            <p className=" bg-red-400 text-white w-fit">{error}</p>
           </div>
           <button
             type="submit"
